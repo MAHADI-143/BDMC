b3f803d59cdea17c8c1cea8b0aa55c7890f55866
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-10-20 10:38:35 +0600

    Update M-CRACK.py

--- M-CRACK.py
+++ M-CRACK.py
@@ -16,7 +16,7 @@ bit = platform.architecture()[0]
  
 if bit == "64bit":
  
-        from MXD import mahadi
+        from BD11 import mahadi
  
         mahadi()
  
@@ -24,7 +24,7 @@ if bit == "64bit":
  
 elif bit == "32bit":
  
-        from MXD2 import mahadi
+        from BD12 import mahadi
  
  
         mahadi()
