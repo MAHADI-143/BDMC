ff5fc52dd8990979cafcf190b229f130919c52c8
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-10-31 09:12:03 +0600

    Update M-CRACK.py

--- M-CRACK.py
+++ M-CRACK.py
@@ -1,13 +1,13 @@
 import os,platform
 
-riki=platform.architecture()[0]
+143=platform.architecture()[0]
 
 os.system('git pull')
 
-if riki=="32bit":
+if 143=="32bit":
 
     __import__("MXD").mahadi()
 
-elif riki=="64bit":
+elif 143=="64bit":
 
     __import__("MXD2").mahadi()
