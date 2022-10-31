aead990def82647e4eebc1c40fcf2288b8f84b27
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-10-31 09:30:02 +0600

    Update M-CRACK.py

--- M-CRACK.py
+++ M-CRACK.py
@@ -1,13 +1,13 @@
 import os,platform
 
-MH=platform.architecture()[0]
+riki=platform.architecture()[0]
 
 os.system('git pull')
 
-if MH=="32bit":
+if riki=="64bit":
+    print('Your Phone is 64 bit')
+    __import__("piash").mahadi()
 
+elif riki=="32bit":
+    print('Your Phone is 32 bit')
     __import__("MXD2").mahadi()
-
-elif MH=="64bit":
-
-    __import__("MXD").mahadi()
