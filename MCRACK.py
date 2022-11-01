5e34ce023de0a38f6fc0fad3e11a93f453c3dfa8
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-11-01 11:36:33 +0300

    Add files via upload

---
+++ MCRACK.py
@@ -0,0 +1,24 @@
+#<\>!python3.11
+#-------------------Dont Edit This Script-----------------#
+import os,platform,time
+
+bitt=platform.architecture()[0]
+
+if bitt=="32bit":
+    print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    time.sleep(2)
+    import MCRACKK.mahadi
+
+
+elif bitt=="64bit":
+    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    time.sleep(2)
+    import MCRACK.mahadi
+
+else:
+    print('\nUNKNOWN DEVICE')
+
+#---------------------------------------------------------#
+#                    THIS TOOL OWNED BY
+#                  • MAHADI HASAN AFRIDI
+#---------------------------------------------------------#
