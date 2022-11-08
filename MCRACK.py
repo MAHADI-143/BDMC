a5366a38da01bea92d6c20b3f58c0e5873a92085
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-11-08 18:43:29 +0600

    Update MCRACK.py

--- MCRACK.py
+++ MCRACK.py
@@ -13,7 +13,7 @@ if bitt=="32bit":
 elif bitt=="64bit":
     os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import FIRE64.mahadi
+    import SAUWA64.mahadi
 
 else:
     print('\nUNKNOWN DEVICE')
