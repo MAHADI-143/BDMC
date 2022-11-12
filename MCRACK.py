5eba249731b2db7f32b780928dcf32f435d64a81
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-11-12 08:46:52 +0600

    Update MCRACK.py

--- MCRACK.py
+++ MCRACK.py
@@ -7,7 +7,7 @@ bitt=platform.architecture()[0]
 if bitt=="32bit":
     print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import MH32.mahadi
+    import FIRE32.mahadi
 
 
 elif bitt=="64bit":
