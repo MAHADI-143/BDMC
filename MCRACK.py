6cafc780d77050df9efd230f8dd4cb09b28342c3
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2023-04-08 01:41:41 +0400

    Update MCRACK.py

--- MCRACK.py
+++ MCRACK.py
@@ -1,17 +1,22 @@
 #<\>!python3.11
 #-------------------Dont Edit This Script-----------------#
 import os,platform,time
-os.system('git pull')
+
 bitt=platform.architecture()[0]
 
-if bitt=="64bit":
+if bitt=="32bit":
+    print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    time.sleep(2)
+    import RN32.mahadi
+
+
+elif bitt=="64bit":
     os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    os.system('chmod +x mahadi');os.system('./mahadi')
+    import RN.mahadi
 
 else:
-
-    print('\nYOUR DEVICE 32 BIT NOT SUPPORT')
+    print('\nUNKNOWN DEVICE')
 
 #---------------------------------------------------------#
 #                    THIS TOOL OWNED BY
