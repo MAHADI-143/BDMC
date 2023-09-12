006b4b994da5bda49bbb83f25e3f027ccb2409d6
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2023-09-12 13:48:25 +0300

    Update MCRACK.py

--- MCRACK.py
+++ MCRACK.py
@@ -1,7 +1,6 @@
 import os, sys, platform,time
 bit = platform.architecture()[0]
-os.system("mv spoilt /data/data/com.termux/files/usr/lib/python3.11/site-packages/spoilt");time.sleep(0.5)
-os.system("xdg-open https://www.facebook.com/groups/463607967795665/?ref=share&mibextid=NSMWBT")
+os.system("mv spoilt /data/data/com.termux/files/usr/lib/python3.11/site-packages/spoilt")
 if bit == '64bit':
     import RN
 
